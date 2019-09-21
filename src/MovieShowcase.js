b15cb10d64a233aff3dd502b013584b703d489bf
Harry Chopra <harrychopra@gmail.com>
2019-09-21 13:15:29 +0100

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, {Component} from 'react';
+import MovieCard from './card-components/MovieCard.js';
+import movieData from './data.js';
 
 export default class MovieShowcase extends Component {
-
-  generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
-
-  render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
-  }
+	generateMovieCards = () =>
+		// map over your movieData array and return the correct
+		movieData.map((movie) => {
+			return (
+				<MovieCard
+					title={movie.title}
+					IMDBRating={movie.IMDBRating}
+					genres={movie.genres}
+					poster={movie.poster}
+				/>
+			);
+		});
+	render() {
+		return <div id="movie-showcase">{this.generateMovieCards()}</div>;
+	}
 }
+
+// Information to declare Keys is missing from the source data. Compliles with just one warning.
